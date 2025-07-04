d80f5b5fb0c8ba64b237d5b6d3734d4f8243a99f
Abdülbaki Demir <baki_demir_bs@hotmail.com>
2025-07-04 22:51:21 +0300

    new commit 04.07.2025-22:51

--- pages/app_rating_page.js
+++ pages/app_rating_page.js
@@ -1,21 +1,32 @@
 // import { PageComponent2 } from "../components/page2.js";
 
+
+const data = [
+    {index:0,id:1,value:0},
+    {index:1,id:4,value:0},
+    {index:2,id:7,value:0},
+    {index:3,id:9,value:0},
+    {index:4,id:123,value:0}
+]
 function createPageComponent(){
     return`
         <div class="main-bg">
             <div class="full-center">
                 <div class="transparent-card">
                     <h1 class="main-title">Welcome to the Rating Page</h1>
-                    <div class="movie-list">
-                        ${imageComponent(1)}
-                        ${imageComponent(2)}
+                    <div class="movie-list" value="${data[0].index}">
+                        ${imageComponent(data[0])}
+                    </div>
+                    <div id="movie-previous-next" class="d-flex justify-content-between">
+                        <button onclick="previous()" id="btn-previous" class="btn-sm btn btn-danger" disabled>Previous</button>
+                        <button onclick="next()" id="btn-next" class="btn-sm btn btn-danger">Next</button>
                     </div>
                     <button class="btn btn-danger" onclick="changePage('')">Go to Main Page</button>
                 </div>
             </div>
         </div>
     `
-}function imageComponent(id){
+}function imageComponent(data){
     return `
         <div class="movie-container mx-auto w-50">
             <div class="movie-image ratio ratio-1x1">
@@ -28,7 +39,7 @@ function createPageComponent(){
                     </div>
                 </div>
             </div>
-            <div id="rating${id}" onload="rating(${id})" value="0" class="rating-stars">
+            <div id="rating${data.id}"  value="${data.value}" class="rating-stars">
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-regular fa-star"></i>
                 <i class="fa-regular fa-star"></i>
@@ -45,40 +56,76 @@ export function AppRatingPage() {
     document.getElementById('app').innerHTML = `
         ${createPageComponent()}
     `;
-
-    function rating(id){
-        document.querySelectorAll( '#rating'+id+' i').forEach((star, index) => {
-            star.addEventListener('click', () => {
-                document.getElementById('rating'+id).setAttribute('value', index);
-                const stars = document.querySelectorAll('#rating'+id+' i');
-                stars.forEach((s, i) => {
-                    s.classList.toggle('fa-solid', i <= index);
-                    s.classList.toggle('fa-regular', i > index);
-                });
-            });
-        });
-        document.querySelectorAll( '#rating'+id+' i').forEach((star, index) => {
-            star.addEventListener('mouseover', () => {
-                const stars = document.querySelectorAll('#rating'+id+' i');
-                stars.forEach((s, i) => {
-                    s.classList.toggle('fa-solid', i <= index);
-                    s.classList.toggle('fa-regular', i > index);
-                });
-            });
-        });
-        document.querySelectorAll( '#rating'+id+' i').forEach((star, index) => {
-            star.addEventListener('mouseout', () => {
-                index = parseInt(document.getElementById('rating'+id).getAttribute('value'));
-                const stars = document.querySelectorAll('#rating'+id+' i');
-                stars.forEach((s, i) => {
-                    s.classList.toggle('fa-solid', i <= index);
-                    s.classList.toggle('fa-regular', i > index);
-                });
-            });
-        });
-        
+    window.next = function(){
+        const movieList = document.querySelector(".movie-list");
+        const index= parseInt(movieList.getAttribute("value"), 10);
+        document.getElementById("btn-next").disabled = false;
+        document.getElementById("btn-previous").disabled = false;
+        movieList.setAttribute("value",index+1);
+        movieList.innerHTML = `
+            ${imageComponent(data[index+1])}
+        `
+        rating(data[index+1])
+        if(index+2 === data.length){
+            document.getElementById("btn-next").disabled = true;
+        } 
     }
-    rating(1);
-    rating(2);
+    window.previous = function(){
+        const movieList = document.querySelector(".movie-list");
+        const index= parseInt(movieList.getAttribute("value"), 10);
+        document.getElementById("btn-next").disabled = false;
+        document.getElementById("btn-previous").disabled = false;
+        movieList.setAttribute("value",index-1);
+        movieList.innerHTML = `
+            ${imageComponent(data[index-1])}
+        `
+        rating(data[index-1])
+        if(index-1 === 0){
+            document.getElementById("btn-previous").disabled = true;
+        }
+    }
+    window.rating = function(movieData){
+        document.querySelectorAll( '#rating'+movieData.id+' i').forEach((star, index) => {
+            star.addEventListener('click', () => {
+                const item = data.find(obj => obj.index === movieData.index);
+                if (item) {
+                item.value = index;
+                }
+                document.getElementById('rating'+movieData.id).setAttribute('value', index);
+                const stars = document.querySelectorAll('#rating'+movieData.id+' i');
+                stars.forEach((s, i) => {
+                    s.classList.toggle('fa-solid', i <= index);
+                    s.classList.toggle('fa-regular', i > index);
+                });
+            });
+        });
+        document.querySelectorAll( '#rating'+movieData.id+' i').forEach((star, index) => {
+            star.addEventListener('mouseover', () => {
+                const stars = document.querySelectorAll('#rating'+movieData.id+' i');
+                stars.forEach((s, i) => {
+                    s.classList.toggle('fa-solid', i <= index);
+                    s.classList.toggle('fa-regular', i > index);
+                });
+            });
+        });
+        document.querySelectorAll( '#rating'+movieData.id+' i').forEach((star, index) => {
+            star.addEventListener('mouseout', () => {
+                index = parseInt(document.getElementById('rating'+movieData.id).getAttribute('value'));
+                const stars = document.querySelectorAll('#rating'+movieData.id+' i');
+                stars.forEach((s, i) => {
+                    s.classList.toggle('fa-solid', i <= index);
+                    s.classList.toggle('fa-regular', i > index);
+                });
+            });
+        });
+        const index = parseInt(document.getElementById('rating'+movieData.id).getAttribute('value'));
+        const stars = document.querySelectorAll('#rating'+movieData.id+' i');
+        stars.forEach((s, i) => {
+            s.classList.toggle('fa-solid', i <= index);
+            s.classList.toggle('fa-regular', i > index);
+        });
+    }
+    rating(data[0])
+
 
 }
